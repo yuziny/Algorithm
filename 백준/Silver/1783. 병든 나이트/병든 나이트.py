31cd997299c3d3ea1b03085ad8b2dd6758b69952
yujin jeong <141795938+yuziny@users.noreply.github.com>
2024-03-07 23:33:03 +0900

    [Silver III] Title: 병든 나이트, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1783. 병든 나이트/병든 나이트.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+
+result = 0
+if n == 1:
+    result = 1
+elif n == 2:
+    if m >= 1 and m <= 6:
+        result = (m+1) // 2
+    elif m >= 7:
+        result = 4
+elif n >= 3:
+    if m <= 6:
+        result = min(m,4)
+    elif m >= 7:
+        result = m - 2
+print(result)
