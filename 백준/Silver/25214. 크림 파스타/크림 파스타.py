38a512a929cb042beb5cdc08cd1abccff6f85414
yujin jeong <141795938+yuziny@users.noreply.github.com>
2024-04-21 21:48:49 +0900

    [Silver IV] Title: 크림 파스타, Time: 444 ms, Memory: 64368 KB -BaekjoonHub

---
+++ 백준/Silver/25214. 크림 파스타/크림 파스타.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [[0, 1000000000]]
+
+for i in range(1, n+1):
+    dp.append([max(dp[i-1][0], arr[i-1]-dp[i-1][1]), min(dp[i-1][1], arr[i-1])])
+    
+for i in range(1, n+1):
+    print(dp[i][0], end=' ')
